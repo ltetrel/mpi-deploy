b4ec6987c678c76c0d13377b79b9cd202aca6e92
ltetrel <loic.tetrel.pro@gmail.com>
2021-07-21 12:46:11 -0400

    typo

--- setup_manager.bash
+++ setup_manager.bash
@@ -4,7 +4,7 @@ sudo apt-get install nfs-kernel-server
 echo "/dat `hostname -I | awk '{print $1}' | grep -Po '(\d+\.){3}'`0/24(rw,sync,no_root_squash,no_all_squash)" | sudo tee -a /etc/exports
 sudo mkdir -p /dat
 sudo chown ubuntu /dat
-duso chmod u+rwx /dat
+sudo chmod u+rwx /dat
 sudo systemctl enable nfs-kernel-server
 sudo systemctl restart nfs-kernel-server
 cp * /dat/
