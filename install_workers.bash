7496701282eb42d80f2735222a795dae09818aa4
ltetrel <loic.tetrel.pro@gmail.com>
2021-07-21 14:16:56 -0400

    issues with log redirection

--- install_workers.bash
+++ install_workers.bash
@@ -5,10 +5,9 @@ IPS=$(tac list_hosts | head -$(expr ${N_HOSTS} - 1))
 MANAGER_IP=$(cat list_hosts | head -1)
 for IP in $IPS
 do
-        scp setup_worker.bash $IP:~/
-        ssh $IP "bash setup_worker.bash ${MANAGER_IP}"
-        ssh $IP "touch /dat/$IP"
-        ssh -n -f $IP "sh -c 'nohup /dat/install_mpi.bash > /dat/mpi_logs_$IP && echo "OpenMPI installed on $IP\!" 2>&1 &'"
-        ssh -n -f $IP "sh -c 'nohup /dat/install_singularity.bash > /dat/sing_logs_$IP && echo "Singularity installed on $IP\!" 2>&1 &'"
+        #scp setup_worker.bash $IP:~/
+        #ssh $IP "bash setup_worker.bash ${MANAGER_IP}"
+        #ssh $IP "touch /dat/$IP"
+        ssh -n -f $IP "sh -c 'nohup /dat/install_mpi.bash > /dev/null 2>&1 &'"
+        ssh -n -f $IP "sh -c 'nohup /dat/install_singularity.bash > /dev/null 2>&1 &'"
 done
-echo ""
