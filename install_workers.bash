2f7e2369e094815ea926c7de8454d4bb57743caf
ltetrel <loic.tetrel.pro@gmail.com>
2021-07-21 13:39:29 -0400

    better logs

--- install_workers.bash
+++ install_workers.bash
@@ -8,7 +8,7 @@ do
         scp setup_worker.bash $IP:~/
         ssh $IP "bash setup_worker.bash ${MANAGER_IP}"
         ssh $IP "touch /dat/$IP"
-        ssh -n -f $IP "sh -c 'nohup /dat/install_mpi.bash > mpi_logs_$IP && echo "OpenMPI installed on $IP\!" 2>&1 &'"
-        ssh -n -f $IP "sh -c 'nohup /dat/install_singularity.bash > sing_logs_$IP && echo "Singularity installed on $IP\!" 2>&1 &'"
+        ssh -n -f $IP "sh -c 'nohup /dat/install_mpi.bash > /dat/mpi_logs_$IP && echo "OpenMPI installed on $IP\!" 2>&1 &'"
+        ssh -n -f $IP "sh -c 'nohup /dat/install_singularity.bash > /dat/sing_logs_$IP && echo "Singularity installed on $IP\!" 2>&1 &'"
 done
 echo ""
